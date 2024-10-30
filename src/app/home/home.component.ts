e13b1224afee51bff4244686819bd5555deaac3d
Marcos Magaña <imarcosmagana@gmail.com>
2024-10-29 19:23:28 -0600

    manejos de servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,36 @@
 import { Component } from "@angular/core";
+import { RopaService } from "../service/ropa.service";
+import internal from "stream";
 
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    providers: [RopaService]
 })
 export class HomeComponent {
     public titulo = 'Página Principal';
+    public listado_ropa: Array<string> = [];
+    public prenda_a_guardar:string = '';
+    public indice:number = 0;
+
+    constructor(
+        private _ropaService: RopaService
+    ) {}
+
+    ngOnInit() {
+        this.listado_ropa = this._ropaService.getRopa();
+        console.log(this._ropaService.prueba('Camiseta'));
+        console.log(this.listado_ropa);
+    }
+
+
+    guadarPrenda() {
+        this._ropaService.addRopa(this.prenda_a_guardar);
+        this.prenda_a_guardar = '';
+    }
+
+    eliminarPrenda(indice:number) {
+        this._ropaService.deleteRopa(indice);
+    }
 }
